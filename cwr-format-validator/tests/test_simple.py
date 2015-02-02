25bea3de2e013c5fe12551d06ac33df4eb5ba175
Bernardo Martínez Garrido <programming@wandrell.com>
2015-02-02 09:38:00 +0100

    Test modules names follow the expected pattern.

--- cwr-format-validator/tests/test_simple.py
+++ cwr-format-validator/tests/test_simple.py
@@ -5,14 +5,12 @@ from models.cwr_objects import CWRField
 
 
 class DocumentRegexTest(unittest.TestCase):
+
     def setUp(self):
         self.file_path = 'test-files/CW1328EMI_059.V21'
 
-    def testi(self):
+    def test_default_value_none(self):
         field = CWRField('Title', regex.get_ascii_regex(60))
         field.value = None
 
-        if field.value is None:
-            print "Yay"
-        else:
-            print "do'h"
+        self.assertIsNone(field.value)
