5f9260cb6bc137a9933c46977aa9eabccb379a10
Bernardo Martínez Garrido <programming@wandrell.com>
2015-02-02 11:28:12 +0100

    Fixed a typo on the requirements script

--- src/run_webapp.py
+++ src/run_webapp.py
@@ -3,8 +3,6 @@ from webapp import app
 __author__ = 'Borja'
 
 if __name__ == '__main__':
-    app.debug = True
-    app.secret_key = "development_key"
     app.run(
         host="127.0.0.1",
         port=int("5001")
--- src/webapp/__init__.py
+++ src/webapp/__init__.py
@@ -4,5 +4,6 @@ __author__ = 'Borja'
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'development_key'
 
 from webapp import views
