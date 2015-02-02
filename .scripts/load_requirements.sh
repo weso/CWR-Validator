5851e248fc0b60d40597b8c292dc3ef30e27a3c3
Bernardo Martínez Garrido <programming@wandrell.com>
2015-02-02 11:47:56 +0100

    Set requirements for Python 3.3

--- .scripts/load_requirements.sh
+++ .scripts/load_requirements.sh
@@ -4,6 +4,10 @@ case "$(python --version 2>&1)" in
     *" 2.6."*)
         pip install -r requirements_2.6.txt
         ;;
+    *" 2.7."*)
+        pip install -r requirements.txt
+        ;;
+    *" 3.3."*)
+        pip install -r requirements_3.3.txt
+        ;;
 esac
-
-pip install -r requirements.txt
